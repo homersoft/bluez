3a7df572f0fdbbfe1e5031d9ca3a1db96238082c
Michał Lowas-Rzechonek <michal.lowas-rzechonek@silvair.com>
2019-08-28 14:55:33 +0200

    mesh: Added mesh-io-tcpserver

--- mesh/mesh-io.h
+++ mesh/mesh-io.h
@@ -29,7 +29,8 @@ struct mesh_io;
 enum mesh_io_type {
 	MESH_IO_TYPE_NONE = 0,
 	MESH_IO_TYPE_GENERIC,
-	MESH_IO_TYPE_SILVAIR
+	MESH_IO_TYPE_SILVAIR,
+	MESH_IO_TYPE_TCPSERVER
 };
 
 enum mesh_io_timing_type {
