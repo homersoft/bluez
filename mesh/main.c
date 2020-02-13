54c85903a8e0dd4abea9baff861452a8d19181b7
Przemysław Fierek <przemyslaw.fierek@silvair.com>
2020-02-13 15:52:10 +0100

    mesh: Add D-Bus API to 'Access Control List' management to the 'mesh-io-tcpserver'
    This patch provides D-Bus API to access control management. From now it
    is possible to grant or revoke access for 'uGW' using single D-Bus call.

--- mesh/main.c
+++ mesh/main.c
@@ -74,7 +74,7 @@ static void usage(void)
 	       "\t\tavailable one\n"
 	       "\tsilvair:<tty>\n"
 	       "\t\tUse Silvair Radio SLIP protocol on <tty>\n"
-	       "\ttcpserver:<port>,<uuid>,<dev-key>,<net-key>\n"
+	       "\ttcpserver:<port>\n"
 	       "\t\tUse Silvair Radio SLIP protocol over TCP/IP\n");
 }
 
